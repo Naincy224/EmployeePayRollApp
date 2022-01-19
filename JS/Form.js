f1feac0242c3037f1efb4e21b4c357083d1177bc
Naincy224 <pihusingh224@gmail.com>
2022-01-19 20:20:41 +0530

    Added: Vaidation to start date and name

--- JS/Form.js
+++ JS/Form.js
@@ -1,28 +1,11 @@
 
+
 window.addEventListener('DOMContentLoaded', (event) => {
     salaryOutput();
     validateName();
-    validateDate();
-
+    validateDate();  
 });
 
-function validateName() {
-    const name = document.querySelector("#name");
-    const textError = document.querySelector(".text-error");
-    name.addEventListener('input', function() {
-        if (name.value.length == 0) {
-            textError.textContent = "";
-            return;
-        }
-        try {
-            (new EmployeePayrollData()).name = name.value;;
-            textError.textContent = "";
-        } catch (e) {
-            console.error(e)
-            textError.textContent = e;
-        }
-    });
-}
 const salaryOutput = () => {
     const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
@@ -31,3 +14,42 @@ const salaryOutput = () => {
         output.textContent = salary.value;
     });
 }
+
+const validateName = () => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if(name.value.length == 0){
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
+        }
+    });
+}
+
+const validateDate = () => {
+    
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+
+    day.addEventListener('input', isValidDate);
+    month.addEventListener('input', isValidDate);
+    year.addEventListener('input', isValidDate);
+
+}
+
+const isValidDate = () => {
+    const dateError = document.querySelector('.date-error');
+    try {
+        (new EmployeePayrollData()).startDate = new Date(month.value+" "+day.value+" "+year.value);
+        dateError.textContent = " ";
+    } catch (e) {
+        dateError.textContent = e ;
+    } 
+ }
